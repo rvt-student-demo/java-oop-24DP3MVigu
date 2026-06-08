c58df373962e318a8845415020caecf70d2bc5c5
24DP3MVigu <a242491mv@rvt.lv>
2026-06-08 08:23:26 +0000

    I owe you uzdevums java mooc 8-2

---
+++ src/main/java/rvt/IOU.java
@@ -0,0 +1,19 @@
+package iou;
+
+import java.util.HashMap;
+
+public class IOU {
+    private HashMap<String, Double> debts;
+
+    public IOU() {
+        debts = new HashMap<>();
+    }
+
+    public void setSum(String toWhom, double amount) {
+        debts.put(toWhom, amount);
+    }
+
+    public double howMuchDoIOweTo(String toWhom) {
+        return debts.getOrDefault(toWhom, 0.0);
+    }
+}
